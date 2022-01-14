275bd95d6fa35f2fabeb441ab88aa7c0b9bfedb1
John-Ingham <johnaingham@yahoo.co.uk>
2022-01-14 10:44:31 +0000

    api/models edited, made floatfield unrestricted

--- sqlite_api/api/models.py
+++ sqlite_api/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models # imports the models class information
-from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Model will save data to the database, using ORM, object relational mapping
 # Avoids writing SQL, Django does this for us.
@@ -14,7 +14,7 @@ class Candidate(models.Model):
 
 
 class Score(models.Model):
-    score = models.FloatField(min = 0.0, max = 100.0, validators = [MinValueValidator(0.0), MaxValueValidator(100.0)]) # Will always be a float number between 1 and 100
+    score = models.FloatField() # Will always be a float number between 1 and 100
     candidate_reference = models.CharField(max_length = 8)
 
     ##Added candidate reference to score class so that a request for all that candidates scores is possible.
