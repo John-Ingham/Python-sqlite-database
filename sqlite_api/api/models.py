8d063b1b8026fe3ac24aa8393a589f4f3aa09e56
John-Ingham <johnaingham@yahoo.co.uk>
2022-01-14 10:29:21 +0000

    Classes - Step1 - completed

--- sqlite_api/api/models.py
+++ sqlite_api/api/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.db import models # imports the models class information
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+# Model will save data to the database, using ORM, object relational mapping
+# Avoids writing SQL, Django does this for us.
+
+## Required classes CANDIDATE and SCORE
+# Candidate will contain `name` and `candidate reference`
+# Score will contain `score`    
+
+class Candidate(models.Model):
+    name = models.CharField(max_length = 255) ## no limit on characters, but prevents long string injection
+    candidate_reference = models.CharField(max_length = 8) # Will always be an unique 8 character code
+
+
+class Score(models.Model):
+    score = models.FloatField(min = 0.0, max = 100.0, validators = [MinValueValidator(0.0), MaxValueValidator(100.0)]) # Will always be a float number between 1 and 100
+    candidate_reference = models.CharField(max_length = 8)
+
+    ##Added candidate reference to score class so that a request for all that candidates scores is possible.
